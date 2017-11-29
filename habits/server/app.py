d5636e93eb6e8ee65bb0b813a2ff2ca71d03aa21
Ruslan Filimonov <rfmind@gmail.com>
2017-11-29 11:08:23 +0100

    Make app creation function-based

--- habits/server/app.py
+++ habits/server/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 
-app = Flask(
-    __name__,
-    static_folder='../static/dist',
-    template_folder='../static')
+def create_app():
+    app = Flask(
+        __name__,
+        static_folder='../static/dist',
+        template_folder='../static')
+    
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/habits/')
-def list_habits():
-    return '[]'
+    @app.route('/habits/')
+    def list_habits():
+        return '[]'
+    
+    return app
