04a613b207c6a377c1e5dbf3c92e8fbc29cb3596
Ruslan Filimonov <rfmind@gmail.com>
2017-12-02 01:40:23 +0100

    Add basic validation

--- habits/server/app/habits_api/views.py
+++ habits/server/app/habits_api/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request, make_response
 from app.habits_api.models import Habit
+from app.habits_api.validation import is_valid
 from app import db
 
 habits_api = Blueprint('habits_api', __name__, url_prefix='/habits')
@@ -24,10 +25,11 @@ def habits():
         return response
     else:
         data = request.get_json(silent=True)
-        if data is None:
+
+        if not is_valid(data):
             return make_response("Bad Request", 400)
- 
-        new_habit = Habit(data['id'], data['name'])
+
+        new_habit = Habit(name=data['name'])
         new_habit.save()
 
         return make_response(habit_to_json(new_habit), 201)
