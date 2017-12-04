7740585fbf0b780daa317b647a8e8f2022eeabd3
Raf Cloesen <rafcloesen@gmail.com>
2017-12-04 18:05:35 +0100

    Remove useless comment.

--- habits/static/src/Main.jsx
+++ habits/static/src/Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-//import HabitsList from './HabitsList.jsx';
 import HabitsTracker from './HabitsTracker.jsx';
 
 render(<HabitsTracker/>, document.getElementById('root'));
