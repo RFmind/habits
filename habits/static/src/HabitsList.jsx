7f8c1115431c98dd6788a1afc074ecb27ed58a36
Raf Cloesen <rafcloesen@gmail.com>
2017-12-06 00:53:03 +0100

    Add delete functionality.

--- habits/static/src/HabitsList.jsx
+++ habits/static/src/HabitsList.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+function deleteHabit(id) {
+    const req = new XMLHttpRequest();
+    req.open('DELETE', '/habits/' + id);
+    req.send(null);
+}
+
 export default class HabitsList extends React.Component {
 
     render() {
@@ -11,10 +17,24 @@ export default class HabitsList extends React.Component {
             return(
                 <ul>
                     {this.props.list.map((item) =>
-                        (<li key={item.id}>{item.name}</li>))}
+                        //(<li key={item.id}>{item.name}</li>))}
+                        (<Habit key={item.id} id={item.id} value={item.name} />))}
                 </ul>
             );
         }
+    }
+};
+
+class Habit extends React.Component {
+
+    render() {
+        return (
+            //<input type="checkbox" value={props.id}>{props.value}<br>
+            <li> 
+                <span onClick={e => deleteHabit(this.props.id)}> X </span>
+                {this.props.value}
+            </li>
+        );
     }
 };
 
