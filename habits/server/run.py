2f60479b77bf92729e6af6060fe67f9bc1ebbb25
Ruslan Filimonov <rfmind@gmail.com>
2017-12-18 19:24:53 +0100

    Use env var for port

--- habits/server/run.py
+++ habits/server/run.py
@@ -3,6 +3,10 @@ from app import create_app
 
 if __name__ == '__main__':
     settings_mode = os.getenv('SETTINGS_MODE')
+    port_env = os.getenv('PORT')
+    if port_env is None:
+        port_env = 4000
+
     app = create_app(settings_mode)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=port_env)
 
