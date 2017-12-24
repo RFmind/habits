53dd474607e6978db25871c2de01ccbdf608894b
Raf Cloesen <rafcloesen@gmail.com>
2017-12-24 11:06:37 +0100

    Refactor Main.jsx to be more compact and expressive.

--- habits/static/src/components/Main.jsx
+++ habits/static/src/components/Main.jsx
@@ -5,22 +5,14 @@ import AddHabit from '../containers/AddHabit'
 import { deleteHabitsRequest } from '../store/actions'
 
 const Main = ({ noHabits }) => {
-    let maybeList
-
-    if (noHabits) {
-        maybeList = (
-            <p className="err">You have not registered any habits yet.</p>
-        )
-    } else {
-        maybeList = (
-            <HabitsList action={deleteHabitsRequest} submitText="Delete" />
-        )
-    }
-
     return (
         <div className="HabitsTracker">
           <header><h1>Habits Tracker</h1></header>
-          {maybeList}
+          {noHabits? (<p className="err">
+                        You have not registered any habits yet.
+                      </p>) :
+                     (<HabitsList action={deleteHabitsRequest}
+                                  submitText="Delete" />)}
           <AddHabit />
         </div>
     )
